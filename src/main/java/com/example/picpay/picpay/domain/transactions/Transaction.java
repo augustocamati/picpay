cf5eaf4a0ecaca6d0b9dce102e8a7424f9328a55
Augusto camati <augustocamati21@gmail.com>
2025-07-18 00:04:32 +0100

    fix: Corrige a relação de transações para ManyToOne entre usuários remetente e destinatário

--- src/main/java/com/example/picpay/picpay/domain/transactions/Transaction.java
+++ src/main/java/com/example/picpay/picpay/domain/transactions/Transaction.java
@@ -5,13 +5,13 @@ import java.time.LocalDateTime;
 
 import com.example.picpay.picpay.domain.user.User;
 
-import jakarta.annotation.Generated;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,11 +28,11 @@ public class Transaction {
   private Long id;
   private BigDecimal amount;
 
-  @OneToMany
+  @ManyToOne
   @JoinColumn(name = "sender_id")
   private User sender;
   
-  @OneToMany
+  @ManyToOne
   @JoinColumn(name = "receiver_id")
   private User receiver;
   
